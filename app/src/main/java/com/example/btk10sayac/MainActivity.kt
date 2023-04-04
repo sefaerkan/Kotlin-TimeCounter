bc923b053ea6c7f6e8d03cb518915c344eb32cdd
Sefa Erkan <erkansefa138@gmail.com>
2023-04-05 02:09:56 +0300

    Time Counter

---
+++ app/src/main/java/com/example/btk10sayac/MainActivity.kt
@@ -0,0 +1,27 @@
+ package com.example.btk10sayac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Abstract Class (Soyut Sınıf)
+        //object ile kullandık
+
+        object : CountDownTimer(15000,1000){
+            override fun onFinish() {
+                textView.text= "Kalan: 0"
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                textView.text= "Kalan: ${millisUntilFinished / 1000}"
+            }
+
+        }.start()
+    }
+}
